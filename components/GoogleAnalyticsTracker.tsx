b7dea816c222780ace4ec570205a5dddb296c88f
vonbro <vonbrother21@gmail.com>
2026-02-26 10:45:25 +0530

    re GA data

--- components/GoogleAnalyticsTracker.tsx
+++ components/GoogleAnalyticsTracker.tsx
@@ -1,27 +1,24 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 declare global {
-  interface Window {
-    gtag: (...args: unknown[]) => void;
-  }
+    interface Window {
+        gtag: (...args: unknown[]) => void;
+    }
 }
 
 export default function GoogleAnalyticsTracker() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+    const pathname = usePathname();
 
-  useEffect(() => {
-    if (!window.gtag) return;
+    useEffect(() => {
+        if (!window.gtag) return;
 
-    const url = pathname + searchParams.toString();
+        window.gtag("event", "page_view", {
+            page_path: pathname,
+        });
+    }, [pathname]);
 
-    window.gtag("event", "page_view", {
-      page_path: url,
-    });
-  }, [pathname, searchParams]);
-
-  return null;
+    return null;
 }
